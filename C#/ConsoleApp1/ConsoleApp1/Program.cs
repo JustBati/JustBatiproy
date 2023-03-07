855985946548e5bf0172749f3faa3aadc3ddb648
Bruno Diaz Medina <brunodiazm300@gmail.com>
2023-03-07 10:10:07 -0600

    Tercer y segudo paso de C# 101

--- C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Primer paso 
+    //Hola mundo
 Console.WriteLine("Hello, World!");
+// Segundo paso 
+    //Variables y uso de ellas para impresion en consola
+String Name = "Jenny Rivera";
+Console.WriteLine($"\tUna artista famosa de banda mexicna fue: {Name}");
+
+//Tercer paso
+    //Uso de metodos
+String Text1 = "En esta parte probamos la concatenacion de cadenas mediante la utilidad de Console.WriteLine";
+Console.WriteLine($"{Text1}\n como a su vez el texto anterior tiene un total de {Text1.Length}");
+
+string Text2 = "[     Aqui tenemos un texto con multiples espacios a la izquierda y la derecha        ]";
+Console.WriteLine(Text2);
+
+
+Console.WriteLine($"{}");
